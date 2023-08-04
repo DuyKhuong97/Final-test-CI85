6e66fc196c4878cf94d6a4c3f363ca74c3c7525d
Duykhuong97 <duykhuong9a@gmail.com>
2023-08-04 21:55:41 +0700

    Final test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TodoListPage from './components/TodoListPage'
+
+function App() {
+
+  return (
+    <div className='Layout'>
+      <TodoListPage />
+    </div>
+    
+  )
+}
+
+export default App
